ceae2154856fb2e50d99c3b0779a9844cf2a814a
shaikrasheed <131394139+Shaikrashe@users.noreply.github.com>
2024-02-29 20:24:42 +0530

    Revert "about loop concept"

--- forloops.py
+++
@@ -1,8 +0,0 @@
-
-# for  lop charactersization
-list="shaikrasheed"
-for char in list:
-    if(char=="a"):
-        print("found",char)
-        print(char)
-        break
--- lopspractice.py
+++
@@ -1,9 +0,0 @@
-
-#given list
-numberlist=[1,4,9,16,25,36,49,64,81,100]
-ind=0
-while ind < len(numberlist):
-   # print(ind) #index print
-    print(numberlist[ind])
-    ind+=1
-
